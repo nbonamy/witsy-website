1a9349d7fc9a53a928a32b1e849e500d91c36d64
Nicolas Bonamy <nicolas@bonamy.fr>
2024-10-12 15:47:36 -0500

    help carousel

--- screens/Help.vue
+++ screens/Help.vue
@@ -13,33 +13,37 @@
       
       <div class="title">How to setup Witsy for OpenAI models</div>
 
-      <div class="box" v-if="activePages.openai == 0">
-        <div class="subtitle">Get your OpenAI API key</div>
-        <ul>
-          <li>Go to <a href="https://platform.openai.com/" target="_blank">OpenAI Platform</a> website</li>
-          <li>Click <b>Log in</b> or <b>Sign up</b> to create an account (upper right corner)</li>
-          <li>If you create a new account, you will have to verify your email address</li>
-          <li>Once logged in, click <b>Dashboard</b> in the horizontal menu (upper right)</li>
-          <li>Click on <b>API keys</b> in the left side menu. You may have to validate your phone number</li>
-          <li>Click <b>+ Create new secret key</b></li>
-          <li>Give it a name (e.g. Witsy) and click <b>Create secret key</b></li>
-          <li>Make sure to <b>Copy</b> your secret key</li>
-        </ul>
-        <div class="nav" @click="goto('openai', 1)"><i class="bi-arrow-right-circle-fill"></i>Next</div>
-      </div>
+      <div class="carousel">
 
-      <div class="box" v-if="activePages.openai == 1">
-        <div class="subtitle">Setup Witsy</div>
-        <ul>
-          <li>Open Witsy</li>
-          <li>Click on the <b>Settings</b> icon in the bottom left corner</li>
-          <li>Click on <b>Models</b></li>
-          <li>Make sure <b>OpenAI</b> is selected in the left hand-side list</li>
-          <li>Paste your OpenAI secret key in the <b>API key</b> field</li>
-          <li>The <b>Chat model</b> list should refresh allowing to select the model of your choice</li>
-          <li>You are all set!. You can close the settings and start using Witsy</li>
-        </ul>
-        <div class="nav" @click="goto('openai', 0)"><i class="bi-arrow-left-circle-fill"></i>Back</div>
+        <div class="box">
+          <div class="subtitle">Get your OpenAI API key</div>
+          <ul>
+            <li>Go to <a href="https://platform.openai.com/" target="_blank">OpenAI Platform</a> website</li>
+            <li>Click <b>Log in</b> or <b>Sign up</b> to create an account (upper right corner)</li>
+            <li>If you create a new account, you will have to verify your email address</li>
+            <li>Once logged in, click <b>Dashboard</b> in the horizontal menu (upper right)</li>
+            <li>Click on <b>API keys</b> in the left side menu. You may have to validate your phone number</li>
+            <li>Click <b>+ Create new secret key</b></li>
+            <li>Give it a name (e.g. Witsy) and click <b>Create secret key</b></li>
+            <li>Make sure to <b>Copy</b> your secret key</li>
+          </ul>
+          <div class="nav" @click="carousels['openai'].next()"><i class="bi-arrow-right-circle-fill"></i>Next</div>
+        </div>
+
+        <div class="box">
+          <div class="subtitle">Setup Witsy</div>
+          <ul>
+            <li>Open Witsy</li>
+            <li>Click on the <b>Settings</b> icon in the bottom left corner</li>
+            <li>Click on <b>Models</b></li>
+            <li>Make sure <b>OpenAI</b> is selected in the left hand-side list</li>
+            <li>Paste your OpenAI secret key in the <b>API key</b> field</li>
+            <li>The <b>Chat model</b> list should refresh allowing to select the model of your choice</li>
+            <li>You are all set! You can close the settings and start using Witsy</li>
+          </ul>
+          <div class="nav" @click="carousels['openai'].previous()"><i class="bi-arrow-left-circle-fill"></i>Back</div>
+        </div>
+
       </div>
 
     </div>
@@ -52,30 +56,35 @@
       
       <div class="title">How to setup Witsy for Ollama models</div>
 
-      <div class="box" v-if="activePages.ollama == 0">
-        <div class="subtitle">Install Ollama</div>
-        <ul>
-          <li>Go to the <a href="https://ollama.com" target="_blank">Ollama homepage</a></li>
-          <li>Click <b>Download ↓</b> to download the latest version of Ollama</li>
-          <li>Execute the downloaded installation program to install Ollama on your computer</li>
-          <li>Launch Ollama</li>
-        </ul>
-        <div class="nav" @click="goto('ollama', 1)"><i class="bi-arrow-right-circle-fill"></i>Next</div>
-      </div>
+      <div class="carousel">
 
-      <div class="box" v-if="activePages.ollama == 1">
-        <div class="subtitle">Setup Witsy</div>
-        <ul>
-          <li>Open Witsy</li>
-          <li>Click on the <b>Settings</b> icon in the bottom left corner</li>
-          <li>Click on <b>Models</b></li>
-          <li>Make sure <b>Ollama</b> is selected in the left hand-side list</li>
-          <li>Select one of the models from <b>Or select one from this list</b></li>
-          <li>Click the <b>Pull</b> button. Warning: this may take some time!</li>
-          <li>You are all set!. You can close the settings and start using Witsy</li>
-          <li>Make sure Ollama is running before you start using Witsy!</li>
-        </ul>
-        <div class="nav" @click="goto('ollama', 0)"><i class="bi-arrow-left-circle-fill"></i>Back</div>
+        <div class="box">
+          
+          <div class="subtitle">Install Ollama</div>
+          <ul>
+            <li>Go to the <a href="https://ollama.com" target="_blank">Ollama homepage</a></li>
+            <li>Click <b>Download ↓</b> to download the latest version of Ollama</li>
+            <li>Execute the downloaded installation program to install Ollama on your computer</li>
+            <li>Launch Ollama</li>
+          </ul>
+          <div class="nav" @click="carousels['ollama'].next()"><i class="bi-arrow-right-circle-fill"></i>Next</div>
+        </div>
+
+        <div class="box">
+          <div class="subtitle">Setup Witsy</div>
+          <ul>
+            <li>Open Witsy</li>
+            <li>Click on the <b>Settings</b> icon in the bottom left corner</li>
+            <li>Click on <b>Models</b></li>
+            <li>Make sure <b>Ollama</b> is selected in the left hand-side list</li>
+            <li>Select one of the models from <b>Or select one from this list</b></li>
+            <li>Click the <b>Pull</b> button. Warning: this may take some time!</li>
+            <li>You are all set! You can close the settings and start using Witsy</li>
+            <li>Make sure Ollama is running before you start using Witsy!</li>
+          </ul>
+          <div class="nav" @click="carousels['ollama'].previous()"><i class="bi-arrow-left-circle-fill"></i>Back</div>
+        </div>
+
       </div>
 
     </div>
@@ -86,19 +95,53 @@
 
 <script setup>
 
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Menubar from '../components/Menubar.vue'
 
-const activePages = ref({
-  openai: 0,
-  ollama: 0
+const carousels = ref({})
+
+onMounted(() => {
+
+  // install carousels
+  for (const el of ['openai', 'ollama']) {
+    carousels.value[el] = bulmaCarousel.attach(`.${el} .carousel`, {
+      initialSlide: 0,
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      infinite: false,
+      pagination: false,
+      navigation: false,
+    })[0]
+
+    // fix carousel transition
+    // otherwise our opacity transition is not working
+    carousels.value[el].on('show', () => {
+      carousels.value[el]._transitioner.end()
+      carousels.value[el]._setClasses()
+    })
+
+  }
 })
 
-const goto = (section, step) => {
-  activePages.value[section] = step
+</script>
+
+<style>
+
+.slider-container {
+  min-height: auto;
+  height: 60%;
+  .slider-item {
+    transition: opacity 0.3s;
+  }
+  .slider-item.is-current {
+    opacity: 1;
+  }
+  .slider-item.is-slide-previous, .slider-item.is-slide-next {
+    opacity: 0;
+  }
 }
 
-</script>
+</style>
 
 <style scoped>
 
@@ -149,3 +192,4 @@ b {
 }
 
 </style>
+
