66b588bca9369605465bb79c900e7843aace50b7
Nicolas Bonamy <nicolas@bonamy.fr>
2024-10-11 21:31:16 -0500

    features rework

--- components/Features.vue
+++ components/Features.vue
@@ -1,86 +1,28 @@
 
 <template>
 
-  <div class="features">
+  <div class='features'>
 
-    <a name="features"></a>
+    <a name='features'></a>
     
-    <div class="title">Features - Click any feature to see it in action!</div>
+    <div class='title'>Features - Click any feature to see it in action!</div>
 
-    <div class="grid feature-list is-col-min-10 is-gap-4">
+    <div class='grid feature-list is-col-min-10 is-gap-4'>
 
-      <div class="cell" @click="showVideo('img/chat.mp4')">
+      <div class='cell' @click='showVideo(`img/${feature.video}`)' v-for='feature in features' :key='feature.video'>
         <Feature>
-          <template v-slot:icon><i class="bi-chat-square"></i></template>
-          <template v-slot:title>Chat</template>
-          <template v-slot:description>
-            Chat is the main window of Witsy. It allows you to interact with all the models in a conversational way. Ask anything you want and the AI will answer.
-            A searchable history of all your conversations is available for you to come back to.
-          </template>
-        </Feature>
-      </div>
-
-      <div class="cell" @click="showVideo('img/scratchpad.mp4')">
-        <Feature>
-          <template v-slot:icon><i class="bi-file-earmark-text"></i></template>
-          <template v-slot:title>Scratchpad</template>
-          <template v-slot:description>
-            The scratchpad is your interactive way to build contents. Just give instructions to the AI model such as "add a title" or "add a paragraph" and it will do it for you.
-            You can also use the magic action bar for quick actions.
-          </template>
-        </Feature>
-      </div>
-
-      <div class="cell" @click="showVideo('img/commands.mp4')">
-        <Feature>
-          <template v-slot:icon><i class="bi-magic"></i></template>
-          <template v-slot:title>AI Commands</template>
-          <template v-slot:description>
-            AI commands are a set of predefined actions that you can trigger with a simple keyboard shortcut. From any other application, Witsy will analyze the text you are working on,
-            execute the command and paste the result back in your application!
-          </template>
-        </Feature>
-      </div>
-
-      <div class="cell" @click="showVideo('img/anywhere.mp4')">
-        <Feature>
-          <template v-slot:icon><i class="bi-terminal"></i></template>
-          <template v-slot:title>Prompt Anywhere</template>
-          <template v-slot:description>
-            Prompt Anywhere allows you to trigger an AI model from any application. Prompt the AI model with the content you want it to create and it will paste the result directly in your application.
-            You can then use AI commands to refine it if needed!
-          </template>
-        </Feature>
-      </div>
-
-      <div class="cell" @click="showVideo('img/experts.mp4')">
-        <Feature>
-          <template v-slot:icon><i class="bi-person-vcard"></i></template>
-          <template v-slot:title>Experts</template>
-          <template v-slot:description>
-            Experts are a set of predefined prompts that will enable you to get the best of the models. From financial advisor to creative writer, you can choose the expert that fits your needs.
-            And if you do not find the expert you need, you can create your own!
-          </template>
-        </Feature>
-      </div>
-
-      <div class="cell" @click="showVideo('img/voice.mp4')">
-        <Feature>
-          <template v-slot:icon><i class="bi-mic"></i></template>
-          <template v-slot:title>Voice</template>
-          <template v-slot:description>
-            Voice is the feature that allows you to interact with the AI models using your voice. Just click the microphone icon and start talking. In conversation mode,
-            the AI model responses will be spoken back to you and you can continue the conversation!
-          </template>
+          <template v-slot:icon><i :class='feature.icon'></i></template>
+          <template v-slot:title>{{ feature.title }}</template>
+          <template v-slot:description>{{ feature.description }}</template>
         </Feature>
       </div>
 
     </div>
 
-    <div class="modal is-active" v-if="video">
-      <div class="modal-background" @click="stopVideo()"></div>
-      <div class="modal-content">
-        <AutoplayVideo id="feature" :src="video" />
+    <div class='modal is-active' v-if='video'>
+      <div class='modal-background' @click='stopVideo()'></div>
+      <div class='modal-content'>
+        <AutoplayVideo id='feature' :src='video' />
       </div>
     </div>
 
@@ -93,6 +35,45 @@
 import { ref } from 'vue'
 import Feature from './Feature.vue'
 import AutoplayVideo from './AutoplayVideo.vue'
+
+const features = ref([
+  {
+    title: 'Chat',
+    icon: 'bi-chat-square',
+    description: 'Chat is the main window of Witsy. It allows you to interact with all the models in a conversational way. Ask anything you want and the AI will answer. A searchable history of all your conversations is available for you to come back to.',
+    video: 'chat.mp4',
+  },
+  {
+    title: 'Scratchpad',
+    icon: 'bi-file-earmark-text',
+    description: 'The scratchpad is your interactive way to build contents. Just give instructions to the AI model such as "add a title" or "add a paragraph" and it will do it for you. You can also use the magic action bar for quick actions.',
+    video: 'scratchpad.mp4',
+  },
+  {
+    title: 'AI Commands',
+    icon: 'bi-magic',
+    description: 'AI commands are a set of predefined actions that you can trigger with a simple keyboard shortcut. From any other application, Witsy will analyze the text you are working on, execute the command and paste the result back in your application!',
+    video: 'commands.mp4',
+  },
+  {
+    title: 'Prompt Anywhere',
+    icon: 'bi-terminal',
+    description: 'Prompt Anywhere allows you to trigger an AI model from any application. Prompt the AI model with the content you want it to create and it will paste the result directly in your application. You can then use AI commands to refine it if needed!',
+    video: 'anywhere.mp4',
+  },
+  {
+    title: 'Experts',
+    icon: 'bi-person-vcard',
+    description: 'Experts are a set of predefined prompts that will enable you to get the best of the models. From financial advisor to creative writer, you can choose the expert that fits your needs. And if you do not find the expert you need, you can create your own!',
+    video: 'experts.mp4',
+  },
+  {
+    title: 'Voice',
+    icon: 'bi-mic',
+    description: 'Voice is the feature that allows you to interact with the AI models using your voice. Just click the microphone icon and start talking. In conversation mode, the AI model responses will be spoken back to you and you can continue the conversation!',
+    video: 'voice.mp4',
+  }
+])
 
 const video = ref(null)
 
